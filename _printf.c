7dd81b04b685a128153cfde73e21d6e2bb5854b1
Chisom Anaele <anaelechisom16@gmail.com>
2022-07-12 11:48:21 -0700

    fixed errors and added descriptions

--- _printf.c
+++ _printf.c
@@ -1,70 +1,75 @@
 #include "main.h"
 /**
- * _prinf - prints inputer characters
- * @*format: number of characters passed to the function
+ * _printf - prints inputer characters
+ * @format: number of characters passed to the function
  * @...: characters to be printed
  * Return: Prints inputed characters and returns the count
  */
 
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    const char *ptr;
-    int length, count;
-    count = 0;
-    
-    ptr = format;
+	va_list ap;
+	const char *ptr;
+	int length, count;
 
-    va_start(ap, *format);
-    while (*ptr)
-    {
-        if (*ptr == '%')
-        {
-            ptr++;
-            if (*ptr == 'c')
-            {
-                char a = va_arg(ap, int);
-                length = _putchar(a);
-                count += length;
-            }
-            else if (*ptr == 'd')
-            {
-                int b = va_arg(ap, int);
-                length = counter(print_num(b));
-                count += length;
-            }
-            else if (*ptr == 'i')
-            {
-                int b = va_arg(ap, int);
-                length = counter(print_num(b));
-                count += length;
-            }
-            else if (*ptr == 's')
-            {
-                char *s = va_arg(ap, char *);
-                length = string(s);
-                count += length;
-            }
-            else if (*ptr == 'x')
-            {
-                long unsigned int h = va_arg(ap, long unsigned int);
-                length = countWrapper(h, 0);
-                count += length;
-            }
-            else if (*ptr == 'X')
-            {
-                long unsigned int h = va_arg(ap, long unsigned int);
-                length = countWrapper(h, 1);
-                count += length;
-            }
-        }
-        else
-        {
-            _putchar(*ptr);
-            count++;
-        }
-        ptr++;
-    }
-    va_end(ap);
-    return (count);
+	count = 0;
+	ptr = format;
+	va_start(ap, *format);
+	while (*ptr)
+	{
+		if (*ptr == '%')
+		{
+			ptr++;
+			if (*ptr == 'c')
+			{
+				char a = va_arg(ap, int);
+
+				length = _putchar(a);
+				count += length;
+			}
+			else if (*ptr == 'd')
+			{
+				int b = va_arg(ap, int);
+
+				length = counter(print_num(b));
+				count += length;
+			}
+			else if (*ptr == 'i')
+			{
+				int b = va_arg(ap, int);
+
+				length = counter(print_num(b));
+				count += length;
+			}
+			else if (*ptr == 's')
+			{
+				char *s = va_arg(ap, char *);
+
+				length = string(s);
+				count += length;
+			}
+			else if (*ptr == 'x')
+			{
+				unsigned long int h = va_arg(ap, unsigned long int);
+
+				length = countWrapper(h, 0);
+				count += length;
+			}
+			else if (*ptr == 'X')
+			{
+				unsigned long int h = va_arg(ap, unsigned long int);
+
+				length = countWrapper(h, 1);
+				count += length;
+			}
+		}
+		else
+		{
+			_putchar(*ptr);
+			count++;
+		}
+		ptr++;
+	}
+	va_end(ap);
+	return (count);
 }
