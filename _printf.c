6c7beddf082ca4f4c60beba9c339ba9400f900c4
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-07-13 00:32:37 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,69 +1,3 @@
-
-#include "main.h"
-
-int _printf(const char *format, ...)
-{
-    va_list ap;
-    const char *ptr; /** pointer to the format */
-    int length, count;
-    count = 0;
-    
-    ptr = format;
-
-    va_start(ap, *format);
-    while (*ptr)
-    {
-        if (*ptr == '%')
-        {
-            ptr++;
-            if (*ptr == 'c')
-            {
-                char a = va_arg(ap, int);
-                length = _putchar(a);
-                count += length;
-            }
-            else if (*ptr == 'd')
-            {
-                int b = va_arg(ap, int);
-                length = counter(print_num(b));
-                count += length;
-            }
-            else if (*ptr == 'i')
-            {
-                int b = va_arg(ap, int);
-                length = counter(print_num(b));
-                count += length;
-            }
-            else if (*ptr == 's')
-            {
-                char *s = va_arg(ap, char *);
-                length = string(s);
-                count += length;
-            }
-            else if (*ptr == 'x')
-            {
-                long unsigned int h = va_arg(ap, long unsigned int);
-                length = countWrapper(h, 0);
-                count += length;
-            }
-            else if (*ptr == 'X')
-            {
-                long unsigned int h = va_arg(ap, long unsigned int);
-                length = countWrapper(h, 1);
-                count += length;
-            }
-        }
-        else
-        {
-            _putchar(*ptr);
-            count++;
-        }
-        ptr++;
-    }
-    va_end(ap);
-    return (count);
-}
-=======
 #include "main.h"
 /**
  * _printf - prints inputer characters and returns the lengths
@@ -139,4 +73,3 @@ int _printf(const char *format, ...)
 	va_end(ap);
 	return (count);
 }
->>>>>>> d70401a65feada18832aff7bc3b93eed4b45f8f0
