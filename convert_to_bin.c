081ec1f30afc10f898568822326c6e3798b45e1b
Chisom Anaele <anaelechisom16@gmail.com>
2022-07-12 09:02:52 -0700

    added descriptions, fixed blank lines, whitespaces indentations and DOS line errors

--- convert_to_bin.c
+++ convert_to_bin.c
@@ -3,22 +3,24 @@
 
 /**
  * convert_to_bin - fuctiontht converts an integer to binary
- * n - integer to convert
+ * @n - integer to convert
  * Return: returns count
  */
 
 long convert_to_bin(int n)
 {
-  long bin = 0;
-  int rem;
-  int i = 1;
-  while (n != 0)
-  {
-    rem = n % 2;
-    n /= 2;
-    bin += rem * i;
-    i *= 10;
-  }
-  print_num(bin);
-  return (counter(bin));
+	long bin = 0;
+	int rem;
+	int i = 1;
+
+	while (n != 0)
+	{
+		rem = n % 2;
+		n /= 2;
+		bin += rem * i;
+		i *= 10;
+	}
+	print_num(bin);
+
+	return (counter(bin));
 }
